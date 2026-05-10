611f0054f4384098dfcde2f3057fd18d9747cf22
Gregor Koprivnikar <gregakop@gmail.com>
2026-05-10 20:12:05 +0200

    responsive

--- resources/js/Pages/Compenzations.vue
+++ resources/js/Pages/Compenzations.vue
@@ -76,7 +76,7 @@
                         {{formatPercentage(compenzation.realization_agreement.commission)}}
                     </td>
                     <td class="pl-6 py-4" @click.stop>
-                        <div class="flex flex-col lg:flex-row lg:flex-wrap items-start lg:items-center gap-1 lg:gap-2">
+                        <div class="flex flex-wrap gap-1.5">
                             <a
                                 v-if="compenzation.proposal && compenzation.proposal.file_path"
                                 :href="route('compenzations.compenzation.pdf.download', { id: compenzation.id, type: 'proposal' })"
